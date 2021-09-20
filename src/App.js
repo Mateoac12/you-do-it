1646656eb208f93dcfab3f550d84afafc6988d26
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-20 20:06:27 -0300

    refact(root): implement router component in app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import ButtonLink from 'components/atoms/ButtonLink'
-import Text from 'components/atoms/Text'
-import Title from 'components/atoms/Title'
-import Target from 'components/layouts/Target'
-import { BUTTON_STYLE, TITLE_STYLE } from 'config/componentsRules'
+import Routers from 'routers/routers'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Title type={TITLE_STYLE.PRINCIPAL}>You do it!</Title>
-      <Text>Busca tu gimnasio y reserva una hora para ir!</Text>
-      <section className='targetsContainer'>
-        <Target />
-      </section>
-      <div className='button-container'>
-        <ButtonLink
-          type={BUTTON_STYLE.SECONDARY}
-          href='/'
-          text='Iniciar sesión'
-        />
-        <ButtonLink href='/' text='Empieza Ya!' />
-        <ButtonLink
-          type={BUTTON_STYLE.TERTIARY}
-          href='/'
-          text='Como funciona la app?'
-        />
-      </div>
+      <Routers />
     </div>
   )
 }
