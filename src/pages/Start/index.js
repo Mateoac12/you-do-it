4a621a74f7fb8bc7f9657c7097e2cf74d887a819
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-20 20:04:23 -0300

    feat(root): new page for create user

---
+++ src/pages/Start/index.js
@@ -0,0 +1,44 @@
+import Text from 'components/atoms/Text'
+import Title from 'components/atoms/Title'
+import { START_TARGET, TITLE_STYLE } from 'config/componentsRules'
+import {
+  BoxTarget,
+  ListOfTargets,
+  ListText,
+  NormalText,
+  SmallText,
+  TitleOfTarget,
+} from './styles'
+
+const Start = () => {
+  return (
+    <>
+      <Title type={TITLE_STYLE.PRINCIPAL}>Comienza!</Title>
+      <Text>Elige un tipo de perfil</Text>
+      <ListOfTargets>
+        <BoxTarget>
+          <SmallText>Deportista</SmallText>
+          <TitleOfTarget>Cuenta de deportista</TitleOfTarget>
+          <NormalText>Con este usuario puedes</NormalText>
+          <ul>
+            <ListText>Ver los cupos libres de tu gimnasio.</ListText>
+            <ListText>Inscribirte a ellos.</ListText>
+          </ul>
+        </BoxTarget>
+        <BoxTarget type={START_TARGET.SECONDATY}>
+          <SmallText>Entrenador</SmallText>
+          <TitleOfTarget>Cuenta de entrenador</TitleOfTarget>
+          <NormalText>Con este usuario puedes</NormalText>
+          <ul>
+            <ListText>
+              Crear una cuenta de Gimnasio y administrar tus usuarios.
+            </ListText>
+            <ListText>Crear cupos por dia y hora.</ListText>
+          </ul>
+        </BoxTarget>
+      </ListOfTargets>
+    </>
+  )
+}
+
+export default Start
