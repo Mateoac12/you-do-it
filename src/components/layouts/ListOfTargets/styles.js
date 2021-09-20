3cdba8ad2498ccac56389ca15d93ef223b4fa545
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-20 20:03:51 -0300

    refact(root): list of targets on home

---
+++ src/components/layouts/ListOfTargets/styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  row-gap: 1rem;
+`
