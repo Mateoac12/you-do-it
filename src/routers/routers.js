02682ef1301a3f025218f8d2956da588634718dd
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-21 18:50:27 -0300

    feat(root): new routers

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,5 +1,7 @@
+import HomeSportPlayer from 'pages/HomeSportPlayer'
 import Landing from 'pages/Landing'
 import Start from 'pages/Start'
+import StartAthlete from 'pages/StartAthlete'
 import { Switch, Route } from 'wouter'
 
 const Routers = () => {
@@ -7,6 +9,8 @@ const Routers = () => {
     <Switch>
       <Route path='/' component={Landing} />
       <Route path='/start' component={Start} />
+      <Route path='/start-as-athlete' component={StartAthlete} />
+      <Route path='/home' component={HomeSportPlayer} />
     </Switch>
   )
 }
