c3e7b0afd919fd9533b28ada411add81a572fa8d
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-23 21:40:48 -0300

    feat(root): add logout button

--- src/pages/HomeSportPlayer/index.js
+++ src/pages/HomeSportPlayer/index.js
@@ -1,6 +1,8 @@
 import ListOfTargets from 'components/layouts/ListOfTargets'
-import logo from 'logo.svg'
-import { useState } from 'react'
+import { Context } from 'context/userContext'
+import { SignOut } from 'firebase/config'
+import { useContext, useState } from 'react'
+import { useLocation } from 'wouter'
 import {
   Avatar,
   ButtonMenu,
@@ -15,25 +17,35 @@ import {
 
 const HomeSportPlayer = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false)
-
+  const [, setLocation] = useLocation()
+  const { user, setUser } = useContext(Context)
   const handleChangeMenu = () => setIsOpenMenu((lastValue) => !lastValue)
+
+  const { displayName = '', photoURL = '' } = user
+
+  const handleLogOut = () => {
+    SignOut()
+    setUser(null)
+    setLocation('/')
+  }
 
   return (
     <HomeContainer>
       <Header>
         <Link href='/user'>
-          <Avatar src={logo} alt='nameOfUser' />
-          <UserName>Mateo Alvarez</UserName>
+          <Avatar src={photoURL} alt={displayName} />
+          <UserName>{displayName}</UserName>
         </Link>
         <ButtonMenu onClick={handleChangeMenu}>
           <LineOfButtons></LineOfButtons>
         </ButtonMenu>
-        {isOpenMenu && <section>information</section>}
+        {isOpenMenu && <section></section>}
       </Header>
       <PrincipalSection>
         <TitleOfList>Reserva un horario</TitleOfList>
         <ListOfTargets />
       </PrincipalSection>
+      <button onClick={handleLogOut}>Salir de aqui</button>
     </HomeContainer>
   )
 }
