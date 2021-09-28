b5025a8aeab803b9d0b83e14609b0ad56fb609aa
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-28 19:15:01 -0300

    feat(Root): New components rules

--- src/config/componentsRules.js
+++ src/config/componentsRules.js
@@ -9,8 +9,15 @@ export const BUTTON_STYLE = {
   PRIMARY: -1,
   SECONDARY: 0,
   TERTIARY: 1,
+  ALTERNATIVE: 2,
 }
 
 export const START_TARGET = {
   SECONDATY: -1,
+}
+
+export const TEXT_STYLE = {
+  PRIMARY: -1,
+  SECONDARY: 0,
+  TERTIARY: 1,
 }
