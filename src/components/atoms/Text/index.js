d664b81428f8f0fd1236eb807a7118ba468007f6
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-28 19:16:52 -0300

    new text rules

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -1,12 +1,14 @@
+import { TEXT_STYLE } from 'config/componentsRules'
 import PropTypes from 'prop-types'
 import { LineText } from './styles'
 
-const Text = ({ children }) => {
-  return <LineText>{children}</LineText>
+const Text = ({ children, type = TEXT_STYLE.PRIMARY }) => {
+  return <LineText type={type}>{children}</LineText>
 }
 
 Text.propTypes = {
   children: PropTypes.string,
+  type: PropTypes.number,
 }
 
 export default Text
--- src/components/atoms/Text/styles.js
+++ src/components/atoms/Text/styles.js
@@ -1,8 +1,31 @@
 import styled from '@emotion/styled'
+import { TEXT_STYLE } from 'config/componentsRules'
 import { styles } from 'config/globalStyles'
 
-export const LineText = styled.p`
+const primary = `
   font-size: 24px;
+`
+
+const secondary = `
+  font-size: 16px;
+`
+
+const tertiary = ``
+
+const allStyles = {
+  [TEXT_STYLE.PRIMARY]: primary,
+  [TEXT_STYLE.SECONDARY]: secondary,
+  [TEXT_STYLE.TERTIARY]: tertiary,
+  default: primary,
+}
+
+const handleSelectStyle = (type) => {
+  return allStyles[type] || allStyles.default
+}
+
+export const LineText = styled.p`
   color: ${styles.whiteAccent};
   margin: 16px 0;
+
+  ${({ type }) => handleSelectStyle(type)};
 `
