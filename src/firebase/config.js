a25059872a3e02e966a71b91f9df67daa3cf9607
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-30 20:56:07 -0300

    ref(Roor): redesign firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,5 @@
-import { initializeApp, getApps } from 'firebase/app'
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  getAuth,
-  signOut,
-  updateProfile,
-} from 'firebase/auth'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC9DnRWHRLrH1xHjD4as8IG02B9_nk1uMw',
@@ -14,47 +8,8 @@ const firebaseConfig = {
   storageBucket: 'you-do-it-617b0.appspot.com',
   messagingSenderId: '298011529462',
   appId: '1:298011529462:web:c9310bfd43cc60b9229cd7',
+  measurementId: 'G-4P27S7FZXZ',
 }
 
-getApps().length === 0 && initializeApp(firebaseConfig)
-
-const auth = getAuth()
-
-export const LoginWithGoogle = () => {
-  const googleProvider = new GoogleAuthProvider()
-
-  return signInWithPopup(auth, googleProvider)
-    .then(async ({ user }) => {
-      updateProfile(auth, {
-        tenantId: 'sportPlayer',
-      }).then(() => {
-        const { uid, photoURL, email, displayName, tenantId } = user
-        console.log({ user })
-        return {
-          uid,
-          photoURL,
-          email,
-          displayName,
-          tenantId,
-        }
-      })
-    })
-    .catch((error) => {
-      const errorCode = error.code
-      const errorMessage = error.message
-      const email = error.email
-      const credential = GoogleAuthProvider.credentialFromError(error)
-
-      console.log({ errorCode, errorMessage, email, credential })
-    })
-}
-
-export const SignOut = () => {
-  signOut(auth)
-    .then(() => {
-      console.log('deslogueo correcto')
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
+firebase.apps.length === 0 && firebase.initializeApp(firebaseConfig)
+export default firebase.firestore()
