31f40a3e970b07e4ee04050b4001dff42ace4abf
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-30 20:55:00 -0300

    ref(Root): change initial object of user context

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export const Context = createContext({ type: 'Not permission' })
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
   console.log('info del user desde el contexto', user)
   return (
     <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
