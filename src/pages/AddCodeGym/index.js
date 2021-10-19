1112095f9e7e0f124343356edb79647b271ae858
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-18 23:58:33 -0300

    feat(root): apply components inside page

--- src/pages/AddCodeGym/index.js
+++ src/pages/AddCodeGym/index.js
@@ -1,10 +1,24 @@
-const { default: Title } = require('components/atoms/Title')
+import ButtonFunction from 'components/atoms/ButtonFunction'
+import Input from 'components/atoms/Input'
+import Title from 'components/atoms/Title'
+import { BUTTON_STYLE, TITLE_STYLE } from 'config/componentsRules'
+import { PageContainer } from 'pages/AboutApp/styles'
 
 const AddCodeGym = () => {
   return (
-    <>
+    <PageContainer>
       <Title>Agregar gimnasio</Title>
-    </>
+      <Title type={TITLE_STYLE.SECONDARY}>
+        Ingrese el nick del gimnasio y su codigo
+      </Title>
+      <Input placeholder='RemoveGym' />
+      <Input placeholder='1423' />
+      <ButtonFunction
+        onClick={() => {}}
+        text='Agregar gimnasio'
+        type={BUTTON_STYLE.SECONDARY}
+      />
+    </PageContainer>
   )
 }
 
