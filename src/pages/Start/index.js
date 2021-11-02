be52e8cfdf6f169e4f707f268ff5fda04371166b
Mateo Álvarez <alvarez.fing@gmail.com>
2021-11-01 23:53:56 -0300

    ref(root): change text

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -1,13 +1,17 @@
 import Text from 'components/atoms/Text'
 import Title from 'components/atoms/Title'
 import ListOfCardSubscriptions from 'components/layouts/ListOfCardSubscriptions'
-import { TITLE_STYLE } from 'config/componentsRules'
+import { TEXT_STYLE, TITLE_STYLE } from 'config/componentsRules'
 
 const Start = () => {
   return (
     <>
       <Title type={TITLE_STYLE.PRINCIPAL}>Comienza!</Title>
-      <Text>Elige un tipo de perfil</Text>
+      <Text>👇🏻 Elige tu tipo de perfil! 👇🏻</Text>
+      <Text type={TEXT_STYLE.SECONDARY}>
+        Si eres socio de un Club debes inscribete como Deportista. Si eres el
+        dueño o el Entrenador, debes inscribirte como Entrenador.
+      </Text>
       <ListOfCardSubscriptions />
     </>
   )
